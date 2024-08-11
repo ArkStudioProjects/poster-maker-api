7986c1a27ce1740aec669c870a14246f9429bbee
Adnan Haque <adnan007.id@gmail.com>
2024-08-11 15:02:13 +0600

    Add thumbnail generation for preview collection

--- app/Models/Design.php
+++ app/Models/Design.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Spatie\MediaLibrary\HasMedia;
 use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
 class Design extends Model implements HasMedia
 {
@@ -20,6 +21,18 @@ class Design extends Model implements HasMedia
         'title',
         'data',
     ];
+
+    public function registerMediaCollections(): void
+    {
+        $this
+            ->addMediaCollection('preview')
+            ->registerMediaConversions( function (Media $media) {
+                $this
+                    ->addMediaConversion('thumbnail')
+                    ->width(119)
+                    ->height(168);
+            } );
+    }
 
     public function author()
     {
